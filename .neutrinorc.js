813adf8ed4205627c82d99ac01579da3264c08f4
ChalkPE <chalk@chalk.pe>
2017-12-20 01:36:08 +0900

    update page title

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -2,6 +2,7 @@ module.exports = {
   use: [
     '@neutrinojs/vue',
     ['@neutrinojs/web', {
+      html: { title: '한스타 사전예약 카운터 by @amato17' },
       devServer: {
         proxy: { '/data': { target: `http://127.0.0.1:8080` } }
       }
